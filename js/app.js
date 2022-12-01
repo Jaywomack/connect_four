914aaa063b1bed1577018f37e0041224be22fe0a
Jaywomack <womackjw@hotmail.com>
2022-12-01 15:42:52 -0800

    created selectSquare function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('connected')
+// DOM SELECTORS
+const squares = document.querySelectorAll('.square')
+
+// Global Variables
+let currentPlayer = 1
+let moves = 0
+
+// Helper functions
+const selectSquare = (event, currentPlayer) => {
+	if (currentPlayer === 0) {
+		event.target.style.color = 'black'
+	} else if (currentPlayer === 1) {
+		event.target.style.color = 'red'
+	}
+}
+
+// Iterate over all squares and attach an click event listener
+squares.forEach((square) => {
+	square.addEventListener('click', (e) => {
+		selectSquare(e, currentPlayer)
+	})
+})
